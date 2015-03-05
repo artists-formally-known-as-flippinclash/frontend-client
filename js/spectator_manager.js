b3d1ed8a56d5f877e07ef30e17857459bd55b4c5
Chae <chaemobile@gmail.com>
2015-03-05 01:48:57 -0500

    Add Pusher, make things sensible

--- js/spectator_manager.js
+++ js/spectator_manager.js
@@ -1,32 +1,8 @@
 (function() {
   function SpectatorManager(){
-    // match manger
-    // idle mode
-    // match making
-    this.round = new Round().connect();
-    this.playerCount = 4
+    this.pusher = new EventListener()
+    alert('Spectator Manager Loaded')
   }
 
-  SpectatorManager.prototype.setup = function() {
-   alert('Loaded!')
-  }
-
-  spectator_manager = new SpectatorManager().setup();
-
-  function Round (){
-    this.state = 'idle'
-  }
-
-  Round.prototype.connect = function(){
-   var pusher = new Pusher('key12355555', {
-     authTransport: 'jsonp',
-     authEndpoint: 'http://mysterious-forest-1231323232323232323232323.herokuapp.com/pusher/auth'
-   }); 
-
-   var channel = socket.subscribe('match') 
-   
-  }
-
+  new SpectatorManager()
 })();
-
-
