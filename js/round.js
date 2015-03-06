ea2731cdc8dc28937f1aca02ba07d976643e1df7
Chae <chaemobile@gmail.com>
2015-03-06 10:15:39 -0500

    refactor - i don't know how to clash

--- js/round.js
+++ js/round.js
@@ -1,55 +1,32 @@
 (function() {
 
-  var register = function(round, data) {
-    var youknowsomething = 5;
-
-      $.ajax({
-        type: "POST",
-        url: round.apiURL + "/matches",
-        data: data,
-
-     }).done(function(data){
-
-     }).error(function(xhr, status, error){
-       console.log(error);
-     });
-  };
-
   function Round() {
-      this.apiURL = "https://blastermind.herokuapp.com";
-      this.testURL = "http://private-fc2d3-blastermind.apiary-mock.com";
-      this.name = "Test";
-      this.player = "";
+    this.apiURL = "https://blastermind.herokuapp.com";
+    this.testURL = "http://private-fc2d3-blastermind.apiary-mock.com";
   }
 
   Round.prototype.registerPlayer = function(name) {
-      alert("Boom! Registered.");
-      var data = { player: { name: name } };
-      var round = this;
-      var ajaxOp = $.ajax({
-        type: "POST",
-        url: round.apiURL + "/matches",
-        data: data,
+    var data = { player: { name: name } };
+    var round = this;
+    var registerPlayerName = $.ajax({
+      type: "POST",
+      url: round.apiURL + "/matches",
+      data: data,
+    });
 
-     })
-
-     ajaxOp.done(function (_dada) {
-        debugger
-     });
-
-     ajaxOp.error(function (xhr, status, error){
-      console.log(error);
-     })
+    registerPlayerName.done(function (_dada) {
+      debugger
+   });
   }
 
-  Round.prototype.startRound = function() {
-      var that = this;
-      $(".new-game").on("mouseup", function(event) {
-          event.preventDefault();
-          var player = that.registerPlayer("hay");
-          console.log(player);
-      });
+  var startRound = function() {
+    var round = new Round();
+    $(".new-game").on("mouseup", function(event) {
+        event.preventDefault();
+        var player = round.registerPlayer("hay");
+        console.log(player);
+    });
   };
 
-  (new Round()).startRound();
+  startRound();
 })();
